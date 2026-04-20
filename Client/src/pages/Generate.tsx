f1a77e09f13733381bbaba8917a888a79bcc09c3
WAJAHAT-ANSARI2 <m.wajahatansari2004@gmail.com>
2026-04-20 16:54:25 +0000

    right side generate page half completed

--- Client/src/pages/Generate.tsx
+++ Client/src/pages/Generate.tsx
@@ -5,6 +5,7 @@ import SoftBackdrop from "../components/SoftBackdrop"
 import AspectRatioSelector from "../components/AspectRatioSelector"
 import StyleSelector from "../components/StyleSelector"
 import ColorSchemeSelector from "../components/ColorSchemeSelector "
+import PreviewPanel from "../components/PreviewPanel"
 
 
 const Genrate = () =>
@@ -66,7 +67,12 @@ return(
                         </div>
                     </div>
                     {/* RIGHT PANEL */}
-                    <div></div>
+                    <div>
+                        <div className="p-6 rounded-2xl bg-white/8 boder border-white/10 shadow-xl">
+                            <h2 className="text-lg font-semibold text-zinc-100 mb-4">Preview</h2>
+                            <PreviewPanel thumbnail={thumbnail} isLoading={loading} aspectRatio={aspectRatio} />
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
